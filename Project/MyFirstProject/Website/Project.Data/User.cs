3e14e44b0d710cd7c9a59ddf208c0eb12e9e3cb6
showmik <bd.showmik@gmail.com>
2018-05-28 16:28:49 +0600

    ASP.NET
    Online Food Ordering Site

---
+++ Project/MyFirstProject/Website/Project.Data/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Entity
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Gender { get; set; }
+        public DateTime LastOnline { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; }
+    }
+}
